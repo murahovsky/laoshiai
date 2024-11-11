4459f7904aa822a8c984ffe8a18af7b8f0b334f8
murahovsky <128924801+murahovsky@users.noreply.github.com>
2024-11-11 20:45:34 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xyzcompany.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // Replace with your key
+const supabaseUrl = 'https://zwwqkwzgncmjxihycijg.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3d3Frd3pnbmNtanhpaHljaWpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEzNTQwMDUsImV4cCI6MjA0NjkzMDAwNX0.tC1qGhgK-j_b17AF804D76TGlP561WmzNEKr9ZpQ3nM'; // Replace with your key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
